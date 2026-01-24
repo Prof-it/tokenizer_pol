726ffc9ede3a62d4f26672c4d7347aab4ed70f68
Rafal Adamczyk <rafal.adamczyk44@gmail.com>
2026-01-24 17:43:53 +0100

    Updating Dataset and args parser

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import pickle
+import numpy as np
 import torch
 import argparse
 from torch.utils.data import Dataset
@@ -8,9 +8,9 @@ from torch.utils.data import random_split
 from config import ModelConfig
 
 
-class WikiDataset(Dataset):
+class CorpusDataset(Dataset):
     def __init__(self, path):
-        self.data = self._load_pkl(path=path)
+        self.data = self._load_data(path=path)
 
     def __getitem__(self, index):
         return torch.tensor(self.data[index], dtype=torch.long)
@@ -18,12 +18,8 @@ class WikiDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
-    def _load_pkl(self, path):
-        """Load tokenized(!) data"""
-        with open(path, 'rb') as f:
-            training_data = pickle.load(f)
-
-        return training_data
+    def _load_data(self, path):
+        return np.load(path, allow_pickle=True)
 
 
 def train_test_split(dataset, train_size):
@@ -44,11 +40,17 @@ def collate_fn(batch):
     return pad_sequence(batch, batch_first=True, padding_value=0)
 
 
+DATA_PATHS = {
+    'spm': 'data/model_training/corpus_spm.npy',
+    'pl': 'data/model_training/corpus_pl.npy',
+}
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Train Polish Language Model')
 
-    # Data paths
-    parser.add_argument('--data_path', type=str, default='training/data/tokenized_polish_wikipedia.pkl', help='Path to tokenized data')
+    # Data mode
+    parser.add_argument('--mode', type=str, choices=['spm', 'pl'], default='pl', help='Tokenizer mode: spm or pl')
 
     # Checkpointing
     parser.add_argument('--resume_from', type=str, default=None, help='Path to checkpoint to resume from')
