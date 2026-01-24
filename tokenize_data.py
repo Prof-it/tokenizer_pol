745a938ea00d4ff7b295c7b556a82132e2ac1b79
Rafal Adamczyk <rafal.adamczyk44@gmail.com>
2026-01-24 17:44:44 +0100

    script for data tokenization/encoding

---
+++ tokenize_data.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('tokenizer/')
+import tokenizer
+import sentencepiece as spm
+import numpy as np
+import json
+
+
+tokenizer_spm = spm.SentencePieceProcessor()
+tokenizer_spm.Load('data/spm/baseline_tokenizer.model')
+
+tokenizer_pl = tokenizer.PolishToKenizer()
+tokenizer_pl.from_pretrained('tokenizer/vocab.json')
+
+with open('data/training/corpus.jsonl', 'r') as f:
+    training_data = [json.loads(line)['text'] for line in f]
+
+
+corpus_spm = [tokenizer_spm.Encode(text) for text in training_data]
+corpus_pl = [tokenizer_pl.encode(text) for text in training_data]
+
+np.save('data/model_training/corpus_spm.npy', np.array(corpus_spm, dtype=object))
+np.save('data/model_training/corpus_pl.npy', np.array(corpus_pl, dtype=object))
+
+print(f"Tokenized {len(training_data)} samples")
+print(f"Saved to data/model_training/corpus_spm.npy and data/model_training/corpus_pl.npy")
