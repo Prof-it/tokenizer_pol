74f0d54671e6944d67ee2b2058348e772b795e5c
Rafal Adamczyk <rafal.adamczyk44@gmail.com>
2026-01-24 17:53:15 +0100

    Adjusting code for cloud training

--- tokenize_data.py
+++ tokenize_data.py
@@ -5,7 +5,6 @@ import sentencepiece as spm
 import numpy as np
 import json
 
-
 tokenizer_spm = spm.SentencePieceProcessor()
 tokenizer_spm.Load('data/spm/baseline_tokenizer.model')
 
@@ -18,10 +17,11 @@ with open('data/training/corpus.jsonl', 'r') as f:
 def encode_datasets():
 
     corpus_spm = [tokenizer_spm.Encode(text) for text in training_data]
-    corpus_pl = [tokenizer_pl.encode(text) for text in training_data]
-
     np.save('data/model_training/corpus_spm.npy', np.array(corpus_spm, dtype=object))
+    print('Saved to data/model_training/corpus_spm.npy')
+
+    corpus_pl = [tokenizer_pl.encode(text) for text in training_data]
     np.save('data/model_training/corpus_pl.npy', np.array(corpus_pl, dtype=object))
+    print('Saved to data/model_training/corpus_pl.npy')
 
     print(f"Tokenized {len(training_data)} samples")
-    print(f"Saved to data/model_training/corpus_spm.npy and data/model_training/corpus_pl.npy")
