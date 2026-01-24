465418e9c8e74cdb9bbd277e73c3ca3e0e1a2c61
Rafal Adamczyk <rafal.adamczyk44@gmail.com>
2026-01-24 17:45:09 +0100

    Script for SPM training

---
+++ train_spm.py
@@ -0,0 +1,21 @@
+import sentencepiece as spm
+
+# 1. train spm on the sama data
+spm.SentencePieceTrainer.train(
+    input='data/training/tokenizer_data.txt',
+    model_prefix='data/spm/baseline_tokenizer',
+    vocab_size=30_000,
+    model_type='bpe',
+    shuffle_input_sentence=True,
+    character_coverage=1.0,
+    split_by_whitespace=True,
+    pad_piece='<pad>',
+    unk_piece='<unk>',
+    bos_piece='<bos>',
+    eos_piece='<eos>',
+    pad_id=0,
+    unk_id=1,
+    bos_id=2,
+    eos_id=3,
+    user_defined_symbols=['<mask>']
+)
